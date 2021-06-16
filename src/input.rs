79e1f6cddb692484032fd6cefe382bb3280b740d
Quentin Hibon <qh.public@yahoo.com>
2021-06-16 21:49:57 +0200

    Don't use ':' in filenames

--- src/input.rs
+++ src/input.rs
@@ -47,7 +47,8 @@ pub fn get_writers(start_time: Timestamp, config: &Config) -> (Vec<Writer>, Vec<
     let summary_out = config.summary_out.as_ref();
     let plan_out = config.plan_out.as_ref();
 
-    let filetime = Local::now().to_rfc3339_opts(SecondsFormat::Nanos, true);
+    // ":" does not seem to work well with Android
+    let filetime = Local::now().to_rfc3339_opts(SecondsFormat::Nanos, true).replace(":", "_");
     let mut plan_writers = vec![];
     let mut summary_writers = vec![];
 
